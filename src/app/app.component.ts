4fb442e423255d8ea9a9aadaa4e4d6d3e1dc0254
Dhondi Srikant <dhondi.srikanth@gmail.com>
2018-08-02 08:17:21 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ExampleDef } from './example.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(
+    private router: Router,
+    @Inject('ExampleDefs') public examples: ExampleDef[]) {
+  }
 }
